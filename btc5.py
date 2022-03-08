4ef581270bf70c0c4dac15efc0a682fc38340901
leesyyy <99805640+leesyyy@users.noreply.github.com>
2022-03-08 21:00:48 +0900

    Create btc5.py

---
+++ btc5.py
@@ -0,0 +1,48 @@
+import time
+import pyupbit
+
+access = ""
+secret = ""
+
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
+#    """현재가 조회"""
+def get_current_price(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+
+# 로그인
+upbit = pyupbit.Upbit(access, secret)
+print("autotrade start")
+
+# 자동매매 시작
+while True:
+    try:
+        ask_size = (pyupbit.get_orderbook(ticker="KRW-BTC")['total_ask_size'])
+        bid_size = (pyupbit.get_orderbook(ticker="KRW-BTC")['total_bid_size']) 
+        current_price = get_current_price("KRW-ETH")
+        avbp = (upbit.get_balances()[1]['avg_buy_price'])
+        avk = float(avbp)*1.005
+        avks = float(avbp)*0.995
+        if ask_size*10 < bid_size and current_price < avks:
+            krw = get_balance("KRW")
+            if krw > 5000:
+                upbit.buy_market_order("KRW-ETH", krw*0.9995)
+
+        if ask_size > bid_size*10 and current_price > avk:
+            coin = get_balance("ETH")
+            if coin > 0.0016:
+                upbit.sell_market_order("KRW-ETH", coin*0.9995)
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        time.sleep(1)
